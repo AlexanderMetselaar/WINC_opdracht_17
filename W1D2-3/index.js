cab7efdfda38768bf4518d18c6fb127bbfb11913
AlexanderMetselaar <80575076+AlexanderMetselaar@users.noreply.github.com>
2021-06-08 18:32:23 +0200

    Add files via upload

---
+++ W1D2-3/index.js
@@ -0,0 +1,7 @@
+// Dit is mijn eerste WINC-JavaScript oefening
+console.log("Hallo Winc Academy Studenten");
+//Dit is een grote som
+console.log(1230941 * 1823794);
+console.log(637263 / 54)
+// Als je van de sommen een comment maakt worden zij niet meer uitgevoerd.
+/* Ook dit is een commentaar */
